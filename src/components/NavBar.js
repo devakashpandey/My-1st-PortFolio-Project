efa66c05444dc0d47414246ea1f6cb0cf269c7ab
DevAkky <developerakky@gmaiil.com>
2023-01-19 18:45:11 +0530

    navbar cpmpleted

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -14,7 +14,9 @@ const NavBar = () => {
         <h1 className='text-5xl font-sign'>Akash</h1>
     </div>
 
-    <ul className='hidden md:flex'>
+    {/* menu for big screen  */}
+
+    <ul className='hidden md:flex text-gray-400'>
       <li 
       className='px-4 cursor-pointer hover:scale-110 font-medium
        duration-200'>Home
@@ -38,9 +40,40 @@ const NavBar = () => {
      
     </ul>
 
-    <div onClick={() => setToggle(!toggle)} className='cursor-pointer pr-4'>
+    <div onClick={() => setToggle(!toggle)} className='cursor-pointer pr-4 z-10 md:hidden'>
           {toggle ? <FaTimes size={32}/> : <FaBars size={32}/>}
     </div>
+
+
+
+       {/* menu for mobile section  */}
+
+       { toggle && (
+                      <ul className='flex flex-col justify-center items-center absolute top-0
+                      left-0 w-full h-screen bg-black text-3xl text-gray-400'>
+                   
+                       <li 
+                       className='py-4 cursor-pointer'>Home
+                       </li>
+                       <li 
+                       className='py-4 cursor-pointer'>About
+                       </li>
+                       <li 
+                       className='py-4 cursor-pointer'>Skills
+                       </li>
+                       <li 
+                       className='py-4 cursor-pointer'>Portfolio
+                       </li>
+                       <li 
+                       className='py-4 cursor-pointer'>Contact
+                       </li>
+                 
+                     </ul>
+               )
+          }
+
+ 
+
 
        </div>
       </>
