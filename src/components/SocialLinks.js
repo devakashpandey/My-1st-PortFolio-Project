8e891a6cf719f509aab667d3508b05bd050fab9f
DevAkky <developerakky@gmaiil.com>
2023-01-21 17:31:40 +0530

    social sidebar done

--- src/components/SocialLinks.js
+++ src/components/SocialLinks.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {FaGithub, FaLinkedin } from "react-icons/fa"
 import { HiOutlineMail } from "react-icons/hi"
-import { BsFillPersonLinesFill } from "react-icons/bs"
+import { BsInstagram } from "react-icons/bs"
 import "./SocialLinks.css"
 
 const SocialLinks = () => {
@@ -9,10 +9,25 @@ const SocialLinks = () => {
      <>
      <div className='social'>
           <ul>
-              <li className='px-4 social-li ml-[-100px]'>
-                      <a href="#" className='w-full social-link'>
+              <li className='px-4 social-li'>
+                      <a href="https://github.com/devakashpandey" target="_github" className='w-full social-link'>
+                        <>GitHub <FaGithub size={28}/> </>
+                      </a>
+              </li>
+              <li className='px-4 social-li'>
+                      <a href="https://www.linkedin.com/in/devakashpandey/" target="_linkedin" className='w-full social-link'>
                         <>LinkedIn <FaLinkedin size={28}/> </>
                       </a>
+              </li>
+              <li className='px-4 social-li'>
+                      <a href="mailto:developerakky@gmail,com" target="_mail" className='w-full social-link'>
+                        <>Mail <HiOutlineMail size={28}/> </>
+                      </a>
+              </li>
+              <li className='px-4 social-li'>
+                      <a href="https://www.instagram.com/codingwithakash" target="_instagram" className='w-full social-link resume'>
+                        <>Instagram <BsInstagram size={28}/> </>
+                      </a> 
               </li>
           </ul>            
      </div>
