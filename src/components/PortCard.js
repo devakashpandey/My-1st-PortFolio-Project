7d20303cba2a7af549bbe7fecaeac53a75385a1f
DevAkky <developerakky@gmaiil.com>
2023-01-22 14:08:49 +0530

    portfolio scetion done

---
+++ src/components/PortCard.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import "./Portcard.css"
+import expenseTracker from "../assets/portfolio/expensetracker.png"
+import filterGallery from "../assets/portfolio/filtgal.png"
+import tictactoe from "../assets/portfolio/tictactoe.png"
+import todoList from "../assets/portfolio/todo.png"
+import weatherApp from "../assets/portfolio/weather.png"
+
+
+const PortCard = () => {
+
+ const portCards = [
+    {
+      id: 1,
+      src: expenseTracker
+    },
+    {
+      id: 2,
+      src: filterGallery
+    },
+    {
+      id: 3,
+      src: tictactoe
+     },
+    {
+      id: 4,
+      src:todoList,
+    },
+    {
+      id: 5,
+      src:weatherApp
+    }
+ ]
+
+  return (
+     <>
+       {/* portfolio card start !!*/}
+
+      <div className='grid sm:grid-cols-2 md:grid-cols-3 sm:px-0 gap-8 px-12 '>
+      
+      {
+        portCards.map(({id, src, height}) =>{
+            return (
+                      <div key={id} className='port-card h-full'>
+                      <img src={src} alt=" expenseTracker" className='port-img duration-300'></img>
+                      
+                      <div className='buttons'>
+                        <button>Demo</button>
+                        <button>Get Code </button>
+                      </div>
+                    </div>
+            )
+        })
+      }
+      
+    
+      </div>
+
+
+    {/* portfolio card end !!!*/}
+     </>
+  )
+}
+
+export default PortCard;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,10 +1,27 @@
 import React from 'react'
 import "./Portfolio.css"
+import PortCard from './PortCard';
 
 function Portfolio() {
   return (
    <>
-   
+      <div name="portfolio" className='main-portfolio'>
+ 
+        <div className='max-w-screen-lg p-4 mx-auto port2'>
+          <div className='pb-8 '>
+            <p className='port-p'>PortFolio</p>
+            <p className='py-6 text-xl'>Check out some of my work right here. </p>
+          </div>
+
+    {/* portfolio card start !!*/}
+
+    <PortCard />
+
+    {/* portfolio card end !!!*/}
+
+      </div>
+
+    </div>
    </>
   )
 }
